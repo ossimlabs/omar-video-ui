4c0d2e169003b9a897e8cb18d3ed3b06d5832a11
knaquin <kevin.naquin@digitalglobe.com>
2019-06-12 15:56:58 -0400

    Testing with new URL.

--- client/src/App.vue
+++ client/src/App.vue
@@ -160,7 +160,7 @@ export default {
       const param = this.$route.params.id
       console.log('param', param)
       const apiUrlProd = 'https://omar-dev.ossim.io/omar-services/videoStreaming?id=' + param
-      const apiUrl = 'http://localhost:8081/omar-services/videoStreaming?id=' + param
+      const apiUrl = 'https://omar-dev.ossim.io/omar-services/videoStreaming?id=' + param
 
       axios.post(apiUrlProd)
         .then(res => {
