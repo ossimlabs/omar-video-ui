815823f33d225cf2c4d0ec21c2eaa6ed0f607f61
knaquin <kevin.naquin@digitalglobe.com>
2019-06-12 14:58:10 -0400

    Making the assets come from a context path.

--- client/config/index.js
+++ client/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/omar-video-vrails',
 
     /**
      * Source Maps
