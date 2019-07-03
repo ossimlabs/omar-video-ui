f965ce6c8fc43e607b27469757313d4f0e6af220
Matthew Esterberg <matthewesterberg@mxradmins-MBP.fios-router.home>
2019-07-02 21:07:41 -0400

    changed omar-base to omar-ossim-base to include ffmpeg lib.

--- server/grails-app/services/omar/video/vrails/ScreenshotService.groovy
+++ server/grails-app/services/omar/video/vrails/ScreenshotService.groovy
@@ -9,7 +9,7 @@ class ScreenshotService {
     def getScreenshot( def params) {
         def localScreenshotDir = grailsApplication.config.screenshot.localScreenshotDir
 
-        File file = File.createTempFile("tmp-omar-vrails-screenshot",".jpg")
+        File file = File.createTempFile("tmp-omar-vrails-screenshot",".jpg", new File('/tmp'))
 
         // Expand this out to include name of video
 //        params.filePath = "${localScreenshotDir}/${params.timestamp}.jpg"
@@ -34,6 +34,9 @@ class ScreenshotService {
         /*def proc = cmdScreenshot.execute()
         proc.consumeProcessOutput()
         proc.waitFor()*/
+
+        // deletes file when the virtual machine terminate
+        // file.deleteOnExit()
 
         [ params: params ]
     }
