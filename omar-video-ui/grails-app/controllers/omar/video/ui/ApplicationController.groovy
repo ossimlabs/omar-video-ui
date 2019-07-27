1b0763c9370af768087174891f1391b70480d646
knaquin <kevin.naquin@digitalglobe.com>
2019-07-26 20:27:34 -0400

    Adding swagger API docs.

--- omar-video-ui/grails-app/controllers/omar/video/ui/ApplicationController.groovy
+++ omar-video-ui/grails-app/controllers/omar/video/ui/ApplicationController.groovy
@@ -3,12 +3,40 @@ package omar.video.ui
 import grails.core.GrailsApplication
 import grails.plugins.*
 
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiImplicitParam
+import io.swagger.annotations.ApiImplicitParams
+import io.swagger.annotations.ApiOperation
+
+
+@Api(value = "/application",
+	 description = "OMAR Video UI REST API"
+)
 class ApplicationController implements PluginManagerAware {
 
     GrailsApplication grailsApplication
     GrailsPluginManager pluginManager
 
+    @ApiOperation(
+		httpMethod = "GET",
+	    produces = 'text/html',
+	    value = "Set UI features from the URL."
+	)
+	@ApiImplicitParams([
+		@ApiImplicitParam(
+			dataType = 'string',
+			defaultValue = '',
+			name = 'filter',
+			paramType = 'query',
+			value = 'Specifies the video search parameters.'
+		)
+	])
     def index() {
-        [grailsApplication: grailsApplication, pluginManager: pluginManager]
+        params.remove( 'action' )
+        params.remove( 'controller' )
+        params.remove( 'format' )
+
+        def queryString = params.collect { key, value -> "${ key }=${ value }" }.join( "&" )
+        redirect( url: "/?${ queryString }" )
     }
 }
