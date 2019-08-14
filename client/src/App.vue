d33e239c6a0c308f6c588da90251ea1e4099c7b1
Matthew Esterberg <matthewesterberg@mxradmins-MBP.fios-router.home>
2019-08-14 17:19:16 -0400

    testing..

--- client/src/App.vue
+++ client/src/App.vue
@@ -137,7 +137,7 @@ export default {
   },
   methods: {
     fetchConfig: function () {
-      const pkiEndpoint = `https://pki-${location.hostname}`
+      const pkiEndpoint = `https://${location.hostname}/omar-video-ui`
 
       console.log('url', location.hostname)
       console.log('${process.env.SERVER_URL}', process.env.SERVER_URL)
